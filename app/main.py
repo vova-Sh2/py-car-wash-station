06998f1326c10d04d40deae4a49ea9981195a26e
Vova <vovakucin082@gmail.com>
2025-10-30 18:16:01 +0100

    cod Write

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ class Car:
     def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
-        self.old_clin_mark = clean_mark
         self.brand = brand
 
 
@@ -15,14 +14,14 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        return car.comfort_class * (self.clean_power - car.old_clin_mark) * self.average_rating / self.distance_from_city_center
+        return car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center
 
     def serve_cars(self, cars: list) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
-                self.wash_single_car(car)
                 income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
         return round(income, 1)
 
     def rate_service(self, nev_rating: int) -> None:
@@ -30,4 +29,5 @@ class CarWashStation:
         self.count_of_ratings += 1
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
